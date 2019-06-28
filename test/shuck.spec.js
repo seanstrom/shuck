1af0e92a316e890003616642f0456bc54702856b
Sean Hagstrom <sean@seanhagstrom.com>
2019-06-28 00:06:23 -0700

    test: add specs for initial shuck implementation

---
+++ test/shuck.spec.js
@@ -0,0 +1,31 @@
+import shuck from '../src/shuck'
+
+describe('Shuck', () => {
+  describe('when given an unnested object', () => {
+    it('returns an object with the same properties with null values', () => {
+      const sample = { a: 'a', b: 'b', c: 'c' }
+      const result = shuck(sample)
+
+      expect(result).toEqual({
+        a: null,
+        b: null,
+        c: null
+      })
+    })
+  })
+
+  describe('when given a nested object', () => {
+    it('returns an object with the same nested properties with null values', () => {
+      const sample = { a: 'a', b: 'b', c: { d: 'd' } }
+      const result = shuck(sample)
+
+      expect(result).toEqual({
+        a: null,
+        b: null,
+        c: {
+          d: null
+        }
+      })
+    })
+  })
+})
